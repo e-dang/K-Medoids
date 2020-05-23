86c130f02d2cf1c06307ba5cbed76a47157da29a
Eric Dang <edang830@gmail.com>
2020-05-23 14:49:32 -0700

    Add separate template param for 2nd iter type

--- include/hpkmediods/distances.hpp
+++ include/hpkmediods/distances.hpp
@@ -7,8 +7,8 @@
 template <typename T>
 struct L2Norm
 {
-    template <typename Iter>
-    T operator()(const Iter p1Begin, const Iter p1End, const Iter p2Begin, const Iter)
+    template <typename Iter1, typename Iter2>
+    T operator()(const Iter1 p1Begin, const Iter1 p1End, const Iter2 p2Begin, const Iter2)
     {
         auto result = std::inner_product(p1Begin, p1End, p2Begin, 0.0, std::plus<>(),
                                          [](const T val1, const T val2) { return std::pow(val1 - val2, 2); });
