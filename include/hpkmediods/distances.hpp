9aadad9ac96b3cb19954da5f8868def80b1e4229
Eric Dang <edang830@gmail.com>
2020-05-24 15:25:14 -0700

    Add const to overload () operator

--- include/hpkmediods/distances.hpp
+++ include/hpkmediods/distances.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
 #include <cmath>
-// #include <functional>
 #include <numeric>
 
 template <typename T>
 struct L2Norm
 {
     template <typename Iter1, typename Iter2>
-    T operator()(const Iter1 p1Begin, const Iter1 p1End, const Iter2 p2Begin, const Iter2)
+    T operator()(const Iter1 p1Begin, const Iter1 p1End, const Iter2 p2Begin, const Iter2) const
     {
         auto result = std::inner_product(p1Begin, p1End, p2Begin, 0.0, std::plus<>(),
                                          [](const T val1, const T val2) { return std::pow(val1 - val2, 2); });
