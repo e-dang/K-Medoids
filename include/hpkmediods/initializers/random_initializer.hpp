d1e47a761143542a2473bc99906dd078a982915b
Eric Dang <edang830@gmail.com>
2020-05-27 21:56:17 -0700

    Reimplement RandomInitializer

--- include/hpkmediods/initializers/random_initializer.hpp
+++ include/hpkmediods/initializers/random_initializer.hpp
@@ -1,54 +1,27 @@
 #pragma once
 
-#include <hpkmediods/finders/centroid_finder.hpp>
 #include <hpkmediods/initializers/interface.hpp>
-#include <hpkmediods/selectors/uniform_selectors.hpp>
+#include <hpkmediods/utils/uniform_selectors.hpp>
 
 namespace hpkmediods
 {
 constexpr char RANDOM_INIT[] = "random";
 
-template <typename T, Parallelism Level, class DistanceFunc>
+template <typename T>
 class RandomInitializer : public IInitializer<T>
 {
 public:
-    T initialize(const Matrix<T>* const data, Matrix<T>& centroids, std::vector<int32_t>& assignments,
-                 std::vector<T>& sqDistances) const override
+    void initialize(const Matrix<T>* const data, Clusters<T>* const clusters,
+                    DistanceMatrix<T>* const distMat) const override
     {
-        selectCentroids(data, centroids);
-        m_finder.findAndUpdateClosestCentroids(data, centroids, assignments, sqDistances);
-        return std::accumulate(sqDistances.begin(), sqDistances.end(), 0.0);
-    }
-
-private:
-    template <Parallelism _Level = Level>
-    std::enable_if_t<_Level == Parallelism::Serial || _Level == Parallelism::MPI> selectCentroids(
-      const Matrix<T>* const data, Matrix<T>& centroids) const
-    {
-        auto selections = m_selector.select(centroids.rows(), data->rows());
+        auto selections = m_selector.select(clusters->maxSize(), data->rows());
         for (const auto& selection : selections)
         {
-            centroids.append(data->crowBegin(selection), data->crowEnd(selection));
-        }
-    }
-
-    template <Parallelism _Level = Level>
-    std::enable_if_t<_Level == Parallelism::OMP || _Level == Parallelism::Hybrid> selectCentroids(
-      const Matrix<T>* const data, Matrix<T>& centroids) const
-    {
-        centroids.resize(centroids.rows());
-        auto selections = m_selector.select(centroids.rows(), data->rows());
-        auto selection  = selections.begin();
-
-#pragma omp parallel for shared(data, centroids, selections), schedule(static)
-        for (int i = 0; i < static_cast<int>(selections.size()); ++i, ++selection)
-        {
-            centroids.set(i, data->crowBegin(*selection), data->crowEnd(*selection));
+            clusters->addCentroid(selection);
         }
     }
 
 private:
     UniformSelector m_selector;
-    CentroidFinder<T, Level, DistanceFunc> m_finder;
 };
 }  // namespace hpkmediods
