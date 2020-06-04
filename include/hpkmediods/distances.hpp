98adba77ddfda434030035bbfb8e7edc0acc79ab
Eric Dang <edang830@gmail.com>
2020-06-03 17:40:47 -0700

    Implement L1Norm

--- include/hpkmediods/distances.hpp
+++ include/hpkmediods/distances.hpp
@@ -15,3 +15,14 @@ struct L2Norm
         return std::sqrt(result);
     }
 };
+
+template <typename T>
+struct L1Norm
+{
+    template <typename Iter1, typename Iter2>
+    T operator()(const Iter1 p1Begin, const Iter1 p1End, const Iter2 p2Begin, const Iter2) const
+    {
+        return std::inner_product(p1Begin, p1End, p2Begin, 0.0, std::plus<>(),
+                                  [](const T val1, const T val2) { return std::abs(val1 - val2); });
+    }
+};
--- tests/hpkmediods/test_distances.cpp
+++ tests/hpkmediods/test_distances.cpp
@@ -12,3 +12,11 @@ BOOST_AUTO_TEST_CASE(test_l2norm)
     std::vector<double> vec2 = { 46.0, -2.0, 1.0, 47.0 };
     BOOST_TEST(distanceFunc(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()) == 63.8592, tt::tolerance(0.01));
 }
+
+BOOST_AUTO_TEST_CASE(test_l1norm)
+{
+    L1Norm<double> distanceFunc;
+    std::vector<double> vec1 = { -1.0, 2.0, 3.0, 4.0 };
+    std::vector<double> vec2 = { 46.0, -2.0, 1.0, 47.0 };
+    BOOST_TEST(distanceFunc(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()) == 96.0, tt::tolerance(0.01));
+}
