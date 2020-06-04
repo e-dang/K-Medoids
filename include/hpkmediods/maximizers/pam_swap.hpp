640d1ee5274315ccbc7e71891af53193a38d93a2
Eric Dang <edang830@gmail.com>
2020-06-03 20:29:56 -0700

    Implement tolerance

--- include/hpkmediods/maximizers/pam_swap.hpp
+++ include/hpkmediods/maximizers/pam_swap.hpp
@@ -12,15 +12,18 @@ class PAMSwap : public IMaximizer<T>
 {
 public:
     void maximize(const Matrix<T>* const data, Clusters<T>* const clusters,
-                  DistanceMatrix<T>* const distMat) const override
+                  const DistanceMatrix<T>* const distMat) const override
     {
+        clusters->template calculateAssignmentsFromDistMat<Level>();
+        auto tolerance = -0.01 * (clusters->getError() / data->numRows());
+
         while (true)
         {
             Matrix<T> dissimilarityMat(clusters->size(), data->rows(), true, std::numeric_limits<T>::max());
             maximizeIter(&dissimilarityMat, data, clusters, distMat);
 
             auto minDissimilarity = *min_element(dissimilarityMat.cbegin(), dissimilarityMat.cend());
-            if (minDissimilarity >= 0)
+            if (minDissimilarity >= tolerance)
                 break;
 
             auto coords = dissimilarityMat.find(minDissimilarity);
@@ -34,7 +37,7 @@ private:
     template <Parallelism _Level = Level>
     std::enable_if_t<_Level == Parallelism::Serial || _Level == Parallelism::MPI> maximizeIter(
       Matrix<T>* const dissimilarityMat, const Matrix<T>* const data, Clusters<T>* const clusters,
-      DistanceMatrix<T>* const distMat) const
+      const DistanceMatrix<T>* const distMat) const
     {
         for (int centroidIdx = 0; centroidIdx < clusters->size(); ++centroidIdx)
         {
@@ -45,9 +48,9 @@ private:
     template <Parallelism _Level = Level>
     std::enable_if_t<_Level == Parallelism::OMP || _Level == Parallelism::Hybrid> maximizeIter(
       Matrix<T>* const dissimilarityMat, const Matrix<T>* const data, Clusters<T>* const clusters,
-      DistanceMatrix<T>* const distMat) const
+      const DistanceMatrix<T>* const distMat) const
     {
-#pragma omp parallel for
+#pragma omp parallel for schedule(static)
         for (int centroidIdx = 0; centroidIdx < clusters->size(); ++centroidIdx)
         {
             maximizeIterImpl(centroidIdx, dissimilarityMat, data, clusters, distMat);
@@ -55,42 +58,35 @@ private:
     }
 
     void maximizeIterImpl(const int centroidIdx, Matrix<T>* const dissimilarityMat, const Matrix<T>* const data,
-                          Clusters<T>* const clusters, DistanceMatrix<T>* const distMat) const
+                          Clusters<T>* const clusters, const DistanceMatrix<T>* const distMat) const
     {
         std::vector<T> totals(data->numRows(), std::numeric_limits<T>::max());
         auto selected = clusters->selected();
 
         for (const auto& candidate : clusters->unselected())
         {
-            if (std::find(selected.cbegin(), selected.cend(), candidate) != selected.cend())
+            std::vector<T> contributionVec;
+            contributionVec.reserve(clusters->numCandidates() - 1);
+            for (const auto& point : clusters->unselected())
             {
-                std::vector<T> contributionVec;
-                contributionVec.reserve(clusters->numCandidates() - 1);
-                for (const auto& point : clusters->unselected())
+                if (point != candidate)
                 {
-                    if (point != candidate && std::find(selected.cbegin(), selected.cend(), point) != selected.cend())
-                    {
-                        auto centroidToPointDist        = distMat->distanceToCentroid(point, centroidIdx);
-                        auto pointToClosestCentroidDist = distMat->distanceToClosestCentroid(point);
-                        auto pointToCandidateDist       = distMat->distanceToPoint(candidate, point);
+                    auto centroidToPointDist        = distMat->distanceToCentroid(point, centroidIdx);
+                    auto pointToClosestCentroidDist = distMat->distanceToClosestCentroid(point);
+                    auto pointToCandidateDist       = distMat->distanceToPoint(candidate, point);
 
-                        if (centroidToPointDist > pointToClosestCentroidDist)
-                        {
-                            contributionVec.emplace_back(
-                              std::min(pointToCandidateDist - pointToClosestCentroidDist, 0.0));
-                        }
-                        else if (centroidToPointDist == pointToClosestCentroidDist)
-                        {
-                            auto range                            = distMat->getAllDistancesToCentroids(point);
-                            auto pointToSecondClosestCentroidDist = getSecondLowest(range.first, range.second);
-                            contributionVec.emplace_back(
-                              std::min(pointToSecondClosestCentroidDist, pointToCandidateDist) -
-                              pointToClosestCentroidDist);
-                        }
+                    if (centroidToPointDist > pointToClosestCentroidDist)
+                        contributionVec.emplace_back(std::min(pointToCandidateDist - pointToClosestCentroidDist, 0.0));
+                    else if (centroidToPointDist == pointToClosestCentroidDist)
+                    {
+                        auto range                            = distMat->getAllDistancesToCentroids(point);
+                        auto pointToSecondClosestCentroidDist = getSecondLowest(range.first, range.second);
+                        contributionVec.emplace_back(std::min(pointToSecondClosestCentroidDist, pointToCandidateDist) -
+                                                     pointToClosestCentroidDist);
                     }
                 }
-                totals[candidate] = std::accumulate(contributionVec.cbegin(), contributionVec.cend(), 0.0);
             }
+            totals[candidate] = std::accumulate(contributionVec.cbegin(), contributionVec.cend(), 0.0);
         }
         dissimilarityMat->set(centroidIdx, totals);
     }
